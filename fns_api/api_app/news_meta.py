7ae2c0830f777dd125ccebd771ff5edf391bd6a4
Dongkwan kim <todoaskit@gmail.com>
2017-02-25 13:44:13 +0000

    add news_meta.py

---
+++ fns_api/api_app/news_meta.py
@@ -0,0 +1,12 @@
+def get_og_meta(news_link):
+    """
+    @param news_link TYPE URL
+    @return TYPE DICT
+    {
+        "title": (TYPE STRING),
+        "description": (TYPE SRING),
+        "image": (TYPE URL)
+    }
+    """
+
+    raise NotImplementedError
