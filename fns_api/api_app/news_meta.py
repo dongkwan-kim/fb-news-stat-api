1e5e80ee27f4495f65466202ed8e9727d9afcfb1
juhojuho <soju@kaist.ac.kr>
2017-02-26 23:45:34 +0900

    Implement news_meta.py (#1)
    * Implement news_meta.py
    
    * Fix minor mistake

--- fns_api/api_app/news_meta.py
+++ fns_api/api_app/news_meta.py
@@ -1,4 +1,8 @@
 from api_app.stat import Link
+import urllib.request
+import urllib.parse
+import re
+
 
 def get_og_meta(news_link):
     """
@@ -6,4 +10,26 @@ def get_og_meta(news_link):
     @return TYPE Link(title, description, image, url)
     """
 
-    raise NotImplementedError
+    title = ''
+    description = ''
+    image = ''
+    url = ''
+    request = urllib.request.Request(
+        news_link, headers={'User-Agent': 'Mozilla/5.0'})
+    response = urllib.request.urlopen(request)
+    for line in response:
+        line = line.decode("utf-8")
+        matchObj = re.match(
+            r'.*[\'\"]og:(\w*)[\'\"].*content.*[\'\"](.*)[\'\"].*', line, re.M | re.I)
+        if matchObj:
+            tagType = matchObj.group(1)
+            if tagType == 'title':
+                title = matchObj.group(2)
+            elif tagType == 'description':
+                description = matchObj.group(2)
+            elif tagType == 'image':
+                image = matchObj.group(2)
+            elif tagType == 'url':
+                url = matchObj.group(2)
+
+    return Link(title, description, image, url)
