e5be3fe234981e9fd8c4839ee23d8bba3b44414f
Dongkwan kim <todoaskit@gmail.com>
2017-02-26 08:59:24 +0000

    create log_parse.py

---
+++ fns_api/api_app/log_parse.py
@@ -0,0 +1,16 @@
+def log_parse(log, stat_type):
+    """
+    @param log TYPE DICT
+    @param stat_type TYPE STRING
+    """
+
+    if stat_type == "portal":
+        pass
+    elif stat_type == "page":
+        pass
+
+def parse_portal(log):
+    raise NotImplementedError
+
+def parse_page(log):
+    raise NotImplementederror
