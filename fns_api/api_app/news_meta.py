d011ab6dbf7bb9016c287c008bfa103b6369b20b
Dongkwan kim <todoaskit@gmail.com>
2017-02-26 04:31:52 +0000

    change return type

--- fns_api/api_app/news_meta.py
+++ fns_api/api_app/news_meta.py
@@ -1,12 +1,9 @@
+from api_app.stat import Link
+
 def get_og_meta(news_link):
     """
     @param news_link TYPE URL
-    @return TYPE DICT
-    {
-        "title": (TYPE STRING),
-        "description": (TYPE SRING),
-        "image": (TYPE URL)
-    }
+    @return TYPE Link(title, description, image, url)
     """
 
     raise NotImplementedError
