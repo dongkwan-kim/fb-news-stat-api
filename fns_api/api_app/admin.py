61acca6bc81ec05598e922959c502788e0e6f944
Dongkwan kim <todoaskit@gmail.com>
2017-02-27 09:41:59 +0000

    add admin with new model

--- fns_api/api_app/admin.py
+++ fns_api/api_app/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
-from api_app.models import UserToken
+from api_app.models import UserToken, NewsScore, EncLog
 
 # Register your models here.
 
 class UserTokenAdmin(admin.ModelAdmin):
     list_display = ("token", "email", "month_count")
 
+class NewsScoreAdmin(admin.ModelAdmin):
+    list_display = ("name", "news_id")
+
+class EncLogAdmin(admin.ModelAdmin):
+    list_display = ("enc_id", "enc_info", "saved_date")
+
+
 admin.site.register(UserToken, UserTokenAdmin)
+admin.site.register(NewsScore, NewsScoreAdmin)
+admin.site.register(EncLog, EncLogAdmin)
